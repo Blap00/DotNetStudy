311aa5dabb6767c863abd101402d310f29bc80af
Blap00 <fa.palma@duocuc.cl>
2024-06-05 02:12:12 -0400

    Post Login + buttonExtensión

---
+++ ASPNetMvCApp/ASPNetMvCApp/Helpers/ButtonExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetMvCApp.Helpers
+{
+    public static class ButtonExtension
+    {
+        public static IHtmlContent SubmitButton(this IHtmlHelper htmlHelper, string name, string value)
+        {
+            string str = $"<input type:'submit' name = '{name}' value='{value}'>";
+            return new HtmlString(str);
+        }
+    }
+}
