98e5f67ea2b0c64c53be0ffa4111b03d0b4ad79e
Blap00 <fa.palma@duocuc.cl>
2024-06-05 03:20:16 -0400

    Apply another validations over Model (Comment validator over Controller...)

--- ASPNetMvCApp/ASPNetMvCApp/Controllers/AccountController.cs
+++ ASPNetMvCApp/ASPNetMvCApp/Controllers/AccountController.cs
@@ -20,6 +20,38 @@ namespace ASPNetMvCApp.Controllers
         [HttpPost]
         public IActionResult UserSignup(LoginModel model)
         {
+            //if (string.IsNullOrEmpty(model.Name))
+            //{
+            //    ModelState.AddModelError("Name", "Please, enter a correct name to it.");
+            //}
+            //if (string.IsNullOrEmpty(model.Username))
+            //{
+            //    ModelState.AddModelError("Username", "Please, enter a correct username to it.");
+            //}
+            //if (string.IsNullOrEmpty(model.Password))
+            //{
+            //    ModelState.AddModelError("Password", "Please, enter a correct password to it.");
+            //}
+            //if (string.IsNullOrEmpty(model.ConfirmPassword))
+            //{
+            //    ModelState.AddModelError("ConfirmPassword", "Please, enter a correct password to it.");
+            //}
+            //if(!string.Equals(model.Password, model.ConfirmPassword))
+            //{
+            //    ModelState.AddModelError("ConfirmPassword", "Please, be aware, the password must match.");
+            //}
+            //if (string.IsNullOrEmpty(model.Contact))
+            //{
+            //    ModelState.AddModelError("Contact", "Please, enter a correct contact to it.");
+            //}
+            //if (!model.Terms)
+            //{
+            //    ModelState.AddModelError("Terms", "Please, agree with TERMS & CONDITION");
+            //}
+            if (ModelState.IsValid)
+            {
+                return Redirect("/Account/Login");
+            }
             return View();
         }
 
--- ASPNetMvCApp/ASPNetMvCApp/Models/LoginModel.cs
+++ ASPNetMvCApp/ASPNetMvCApp/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,17 @@ namespace ASPNetMvCApp.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Please, enter a Name")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "Please, enter an Username")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Please, enter a Password")]
         public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Please, Password must match")]
         public string ConfirmPassword{ get; set; }
+        [RegularExpression("^[2,5,6,9]\\d{9}",ErrorMessage = "Please, enter a correct number")]
         public string Contact { get; set; }
-
+        [Required(ErrorMessage = "Please, acept terms and services")]
         public bool Terms { get; set; }
 
     }
