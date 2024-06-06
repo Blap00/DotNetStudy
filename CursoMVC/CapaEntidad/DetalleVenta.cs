4202af6591a2a5e48472aa9bffa9cc0d6d4721dd
Blap00 <fa.palma@duocuc.cl>
2024-06-06 19:42:54 -0400

    Update & ReDO CursoMVC

---
+++ CursoMVC/CapaEntidad/DetalleVenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class DetalleVenta
+    {
+        public int IdDetalleVenta { get; set; }
+        public Venta oIdVenta { get; set; }
+        public Producto oIdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public int Total { get; set; }
+
+
+    }
+}
---
+++ CursoMVC/CapaEntidad/Distrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class Distrito
+    {
+        public int IdDistrito { get; set; }
+        public string Descripcion { get; set; }
+        public string IdProvincia { get; set; }
+        public string IdDepartamento { get; set; }
+
+    }
+}
