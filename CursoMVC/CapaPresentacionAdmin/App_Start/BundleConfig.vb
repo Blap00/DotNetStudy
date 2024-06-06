f5c06d016834806de7d9abc82a76c00b5314f8e4
Blap00 <fa.palma@duocuc.cl>
2024-06-06 14:27:06 -0400

    Add an new _layout & add CapaDatos

--- CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.vb
+++ CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.vb
@@ -7,20 +7,22 @@ Public Module BundleConfig
         bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
                     "~/Scripts/jquery-{version}.js"))
 
-        bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
-                    "~/Scripts/jquery.validate*"))
+        bundles.Add(New ScriptBundle("~/bundles/complementos").Include(
+                    "~/Scripts/scripts.js",
+                    "~/Scripts/fontawesome/all.min.js"))
 
-        ' Use the development version of Modernizr to develop with and learn from. Then, when you're
-        ' ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
-                    "~/Scripts/modernizr-*"))
+        'bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
+        '            "~/Scripts/jquery.validate*"))
 
-        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
-                  "~/Scripts/bootstrap.js"))
+        '' Use the development version of Modernizr to develop with and learn from. Then, when you're
+        '' ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+        'bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+        '            "~/Scripts/modernizr-*"))
 
-        bundles.Add(New StyleBundle("~/Content/css").Include(
-                  "~/Content/bootstrap.css",
-                  "~/Content/site.css"))
+        bundles.Add(New Bundle("~/bundles/bootstrap").Include(
+                  "~/Scripts/bootstrap.bundle.js"))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include("~/Content/site.css"))
     End Sub
 End Module
 
