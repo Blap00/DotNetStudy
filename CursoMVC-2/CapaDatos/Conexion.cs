4bd7ff8da7274ccf02d3e1eff8387446c38e24c5
Blap00 <fa.palma@duocuc.cl>
2024-06-07 18:55:55 -0400

    UPD MODELS CONNECTIONS & CAPAS

---
+++ CursoMVC-2/CapaDatos/Conexion.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Configuration;
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string cn = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+    }
+}
