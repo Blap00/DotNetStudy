667e5b508d99c319c6472f7ab2c169c4f5157f06
Blap00 <fa.palma@duocuc.cl>
2024-06-12 18:43:53 -0400

    Add content LoadingOverlay + sweetalert

--- CursoMVC-2/MyOwnAPP/App_Start/BundleConfig.cs
+++ CursoMVC-2/MyOwnAPP/App_Start/BundleConfig.cs
@@ -16,6 +16,8 @@ namespace MyOwnAPP
                     "~/Scripts/fontawesome/all.min.js",
                     "~/Scripts/DataTables/jquery.dataTables.js",
                     "~/Scripts/DataTables/dataTables.responsive.js",
+                    "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                    "~/Scripts/sweetalert.min.js",
                     "~/Scripts/scripts.js"
                     ));
 
@@ -33,6 +35,7 @@ namespace MyOwnAPP
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/site.css",
                 "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/sweetalert.css",
                 "~/Content/DataTables/css/responsive.dataTables.css"));
 
         }
